774840e71d4a32be92b07b8ef50a6117bb5919ea
Priom Deb <87948186+PriomDeb@users.noreply.github.com>
2022-11-17 15:20:40 +0600

    Pass data into the template with django for loop

--- base/views.py
+++ base/views.py
@@ -3,9 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 
+rooms = [
+    {'id':1, 'name': 'Learn Python'},
+    {'id':2, 'name': 'Learn Django'},
+    {'id':3, 'name': 'Learn Tkinter'},
+]
+
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'room':rooms})
 
 def room(request):
     return render(request, 'room.html')
