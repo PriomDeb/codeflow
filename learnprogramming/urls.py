23697d853d3b66409ef54da6c46dbd4f8f724f1a
Priom Deb <87948186+PriomDeb@users.noreply.github.com>
2022-11-17 14:40:03 +0600

    Configured urls

--- learnprogramming/urls.py
+++ learnprogramming/urls.py
@@ -1,18 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse("Home Page")
-
-def room(request):
-    return HttpResponse("Room")
-
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('room/', room)
+    path('', include('base.urls'))
 ]
