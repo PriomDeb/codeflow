75fbe1e9cd1297223513222382d72b49563578c2
Priom Deb <87948186+PriomDeb@users.noreply.github.com>
2022-11-17 22:27:39 +0600

    Adding some more tables and relations among tables using ForeignKey

--- base/views.py
+++ base/views.py
@@ -1,26 +1,23 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
 
-rooms = [
-    {'id':1, 'name': 'Learn Python'},
-    {'id':2, 'name': 'Learn Django'},
-    {'id':3, 'name': 'Learn Tkinter'},
-]
+# rooms = [
+#     {'id':1, 'name': 'Learn Python'},
+#     {'id':2, 'name': 'Learn Django'},
+#     {'id':3, 'name': 'Learn Tkinter'},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'room':rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-
-    for i in rooms:
-        if i['id'] == int(pk):
-            # This i is actually a dictionary
-            room = i
+    room = Room.objects.get(id=pk)
     
     context = {'rooms': room}
     return render(request, 'base/room.html', context)
