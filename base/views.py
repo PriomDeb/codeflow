ee7cf483c5254a128c01ebc5af4ca1946210fffb
Priom Deb <87948186+PriomDeb@users.noreply.github.com>
2022-11-21 17:27:23 +0600

    Login

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -11,6 +14,29 @@ from .forms import RoomForm
 #     {'id':2, 'name': 'Learn Django'},
 #     {'id':3, 'name': 'Learn Tkinter'},
 # ]
+
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist")
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            # Creates a session and a database in the browser
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Username or password does not exist")
+
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 
 def home(request):
