4bc3a26720dc0c68de1779eb59c9c0434f82b121
Priom Deb <87948186+PriomDeb@users.noreply.github.com>
2022-11-29 22:16:12 +0600

    The whole project is complete

--- base/forms.py
+++ base/forms.py
@@ -4,6 +4,7 @@ from .models import Room
 
 class RoomForm(ModelForm):
     class Meta:
+        # This will render a form
         model = Room
         fields = '__all__'
         exclude = ['host', 'participants']
