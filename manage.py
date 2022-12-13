1e733c473a23357f97dd5230f350a47c01e07a17
Priom Deb <87948186+PriomDeb@users.noreply.github.com>
2022-12-13 11:48:49 +0600

    Updated

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+# python manage.py startapp base
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -7,7 +9,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learnprogramming.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'learnprogramming.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
